5fbac7e79b90231cff8d4869dd3f299d87eedda1
JoannaPichon <77265941+JoannaPichon@users.noreply.github.com>
2021-01-22 11:34:14 +0100

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+let aiguilleDesHeures = document.querySelector('#heure');
+let aiguilleDesMinutes = document.querySelector('.aiguilles .minute');
+let aiguilleDesSecondes = document.querySelector('.aiguilles .seconde');
+
+window.setInterval( function horloge() {
+	let today = new Date();
+	let heuresActuel = today.getHours();
+	let minutesActuel = today.getMinutes();
+	let secondesActuel = today.getSeconds();
+	let hoursAngle = heuresActuel * 30;
+	let minutesAngle = minutesActuel * 6;
+	let secondesAngle = secondesActuel * 6;
+	aiguilleDesHeures.style.transform = 'rotate('+hoursAngle+'deg)';
+	aiguilleDesMinutes.style.transform = 'rotate('+minutesAngle+'deg)';
+	aiguilleDesSecondes.style.transform = 'rotate('+secondesAngle+'deg)';
+}
+	, 500 );
